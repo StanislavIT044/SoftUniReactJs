ebc0fe71f6439f0003729a3a36316903908c55bb
slavi044 <slavi044@abv.bg>
2022-12-28 19:10:14 +0200

    work on components

--- 01.ReactJSX/website-react-demo/src/App.js
+++ 01.ReactJSX/website-react-demo/src/App.js
@@ -1,9 +1,37 @@
-import { HeroSection } from './HeroSection'
+import { HeroSection } from './components/HeroSection'
+import { About } from './components/About'
+import { Service } from './components/Service'
+import { Portfolio } from './components/Portfolio'
+import { News } from './components/News'
+import { Subscribe } from './components/Subscribe'
+import { Client } from './components/Client'
+import { Contact } from './components/Contact'
+import { Info } from './components/Info'
+import { Footer } from './components/Footer'
+
 
 function App() {
     return (
         <div>
             <HeroSection />
+
+            <About />
+
+            <Service />
+
+            <Portfolio />
+
+            <News />
+
+            <Subscribe />
+
+            {/* <Client />
+
+            <Contact />
+            
+            <Info /> */}
+
+            <Footer />
         </div>
     );
 }
